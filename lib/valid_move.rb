b150f425c8c1b90cb4940f54e1a5bfeb374af62d
Stephanie Rhoderick <stephanie.rhoderick@gmail.com>
2018-12-28 17:52:09 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 def valid_move? (board, index)
   def position_taken? (board, index)
-    if board[index] == " " || "" || nil
+    if board[index] == " " || board[index] == "" || board[index] == nil
       return false
     else
       return true
